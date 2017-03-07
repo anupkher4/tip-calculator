5d46ab1819d1faa74f60183fb53572efcb4cd71a
Anup Kher <anup.kher.1990@gmail.com>
2017-03-07 15:45:37 -0800

    Adding basic functional interface

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TipViewController: UIViewController {
+    @IBOutlet weak var tipPercentSegment: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var splitForTwoLabel: UILabel!
+    @IBOutlet weak var splitForFourLabel: UILabel!
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    let tipAmounts = [15.0, 18.0, 20.0]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func tipPercentChanged(_ sender: Any) {
+        amountChanged(sender)
+    }
 
+    @IBAction func amountChanged(_ sender: Any) {
+        guard let text = amountTextField.text else {
+            return
+        }
+        guard let amount = Double(text) else {
+            return
+        }
+        let tip = tipAmounts[tipPercentSegment.selectedSegmentIndex] / 100
+        let total = amount + (amount * tip)
+        totalLabel.text = "= \(String(format: "%.2f", total))"
+        splitForTwoLabel.text = "Split for 2: \(String(format: "%.2f", total / 2))"
+        splitForFourLabel.text = "Split for 4: \(String(format: "%.2f", total / 4))"
+    }
 
 }
 
