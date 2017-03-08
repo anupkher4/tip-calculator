6e053b2090f71e8e871b5d1c6b721e649ed4e9cf
Anup Kher <anup.kher.1990@gmail.com>
2017-03-07 23:33:59 -0800

    Adding a color scheme

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
+enum ColorPalette {
+    case wheat, paleGold, darkCoral, isabellineGray, richBlack
+    
+    func getColor() -> UIColor {
+        switch self {
+        case .wheat:
+            return UIColor(colorLiteralRed:0.96, green:0.89, blue:0.70, alpha:1.0)
+        case .paleGold:
+            return UIColor(colorLiteralRed:0.94, green:0.78, blue:0.55, alpha:1.0)
+        case .darkCoral:
+            return UIColor(colorLiteralRed:0.81, green:0.36, blue:0.21, alpha:1.0)
+        case .isabellineGray:
+            return UIColor(colorLiteralRed: 0.93, green: 0.93, blue: 0.93, alpha: 1.0)
+        case .richBlack:
+            return UIColor(colorLiteralRed: 0.02, green: 0.02, blue: 0.09, alpha: 1.0)
+        }
+    }
+}
+
 class TipViewController: UIViewController {
+    @IBOutlet weak var totalSubView: UIView!
     @IBOutlet weak var tipPercentSegment: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var amountTextField: UITextField!
@@ -25,8 +45,13 @@ class TipViewController: UIViewController {
         
         // Initially hide non-essential elements
         tipPercentSegment.isHidden = true
-        tipAmountLabel.isHidden = true
-        totalLabel.isHidden = true
+        totalSubView.isHidden = true
+        
+        navigationController?.navigationBar.barTintColor = ColorPalette.isabellineGray.getColor()
+        navigationController?.navigationBar.tintColor = ColorPalette.richBlack.getColor()
+        view.backgroundColor = ColorPalette.wheat.getColor()
+        totalSubView.backgroundColor = ColorPalette.paleGold.getColor()
+        tipPercentSegment.tintColor = ColorPalette.darkCoral.getColor()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,8 +76,7 @@ class TipViewController: UIViewController {
         
         // Show hidden elements after begin typing
         tipPercentSegment.isHidden = false
-        tipAmountLabel.isHidden = false
-        totalLabel.isHidden = false
+        totalSubView.isHidden = false
         
         guard let text = amountTextField.text else {
             return
